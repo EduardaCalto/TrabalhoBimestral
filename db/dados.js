b94a6fb369994561a7ed19e353aa89fd19474915
Eduarda <dudacalto@hotmail.com>
2025-05-28 10:48:16 -0300

    .

---
+++ db/dados.js
@@ -0,0 +1,14 @@
+{
+  "usuarios": [
+    { "id": 1, 
+      "login": "admin", 
+      "senha": "1234", 
+      "nome": "Administrador" 
+    }
+  ],
+  "clientes": [],
+  "produtos": [],
+  "fornecedores": [],
+  "entregadores": [],
+  "categorias": []
+}
